db63bef34f2170d3dcb9f132b766a190cda0de18
Zepth <taitanerenwearenumber@gmail.com>
2025-08-11 20:03:49 +0700

    Create speedmirage.lua

---
+++ speedmirage.lua
@@ -0,0 +1,36 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local chr = player.Character
+local hrp = chr:WaitForChild("HumanoidRootPart")
+
+local function getpos()
+    return hrp.CFrame
+end
+
+local function mirage(state)
+    local running = state
+    
+    while running do task.wait()
+        hrp.CFrame = getpos().RightVector * 5
+        task.wait()
+        hrp.CFrame = getpos().LeftVector * 10 
+    end
+end
+
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
+local w = library:CreateWindow("speed mirage") -- Creates the window
+
+local b = w:CreateFolder("B") -- Creates the folder(U will put here your buttons,etc)
+
+b:Label("speed mirage",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+    
+}) 
+
+b:Toggle("Toggle Speed Mirage",function(v)
+    shared.toggle = v
+    mirage(shared.toggle)
+end)
